34d333c965b352bf47b7624649db903bd5a4444b
ALexei Ivanov <alex.ivanov0615@gmail.com>
2023-02-26 23:52:23 -0500

    ..

--- indexing/indexers/dpr_indexer.py
+++ indexing/indexers/dpr_indexer.py
@@ -1,5 +1,6 @@
 from indexing.indexers.indexer import Indexer
 from transformers import DPRContextEncoder, DPRContextEncoderTokenizer
+from torch.nn.functional import normalize 
 
 class DPRIndexer(Indexer):
     def __init__(self, index_name: str, data: list):
@@ -11,7 +12,7 @@ class DPRIndexer(Indexer):
     def gen_data(self):
         for item in self.data:
             input_ids = self.tokenizer(item["courseDescription"], return_tensors="pt")["input_ids"]
-            encoding = self.encoder(input_ids).pooler_output.tolist()[0]
+            encoding = normalize(self.encoder(input_ids).pooler_output).tolist()[0]
             item["courseDescEncoding"] = encoding
             yield {
                 "_index": self.index_name,
