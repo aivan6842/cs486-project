05e5452e73b1df889832a2b0a00fde1186c9357e
Alaattini <alaattini@gmail.com>
2023-03-26 11:36:48 -0400

    modified infer.py file
    added BERT and ELECTRA, but Electra seems to have some issues when "python .\inference\infer.py -q "computer science" -rt "bm25" -n 5 -rr True -ra "electra"" executed, please see my email,

--- inference/infer.py
+++ inference/infer.py
@@ -4,8 +4,10 @@ from inference.retrievers.bm25_retriever import BM25Retriever
 from inference.retrievers.dpr_retriever import DPRRetriever
 from inference.retrievers.t5_retriever import T5Retriever
 from inference.rerankers.miniLMV2_reranker import MiniLMV2Reranker
+from inference.rerankers.BERT_reranker import BERTReranker
+from inference.rerankers.ELECTRA_reranker import ELECTRAReranker
 
-def inference(query: str, retriever_names: list, k: int = 5, rerank: bool = False) -> list[tuple[str, str, str]]:
+def inference(query: str, retriever_names: list, k: int = 5, rerank: bool = False, ranker: str = 'minilm') -> list[tuple[str, str, str]]:
     """
     Given a list of retrievers will return top k course suggestions
     Returns : [(course_code, course_name, course_description), ...]
@@ -33,10 +35,16 @@ def inference(query: str, retriever_names: list, k: int = 5, rerank: bool = Fals
     if len(retriever_names) > 1:
         rerank = True
     
+    RERANKER_MAP = {
+        "minilm" : MiniLMV2Reranker,
+        "bert" : BERTReranker,
+        "electra": T5Retriever
+    }
+
     # unique results only
     reranked_res = list(set(unranked_res))
     if rerank:
-        reranker = MiniLMV2Reranker()
+        reranker = RERANKER_MAP[ranker]()
         reranked_res = reranker.rerank(data=reranked_res, query=query)
     
     return reranked_res[:k]
@@ -65,13 +73,19 @@ if __name__ == "__main__":
                         default=False,
                         help="Rerank documents using re-ranker. Automatically enabled if more than 1 retriever",
                         required=False)
+    parser.add_argument("-ra", "--ranker",
+                        type=str,
+                        default='minilm',
+                        help="type of re-ranker. Automatically enabled if more than 1 retriever",
+                        required=False)
     
     args = parser.parse_args()
 
     res = inference(query=args.query, 
               retriever_names=args.retrievers, 
               k=args.num_results,
-              rerank=args.rerank)
+              rerank=args.rerank,
+              ranker=args.ranker)
     
     print(res)
 
